fd0fe049e3932cf22d23bf6e5e8387cfa61eb001
Puneet Sran <puneet_sran@icloud.com>
2019-11-25 22:30:57 +0000

    add forked reference

--- db/migrate/20191125191033_add_forked_from_id_to_patterns.rb
+++ db/migrate/20191125191033_add_forked_from_id_to_patterns.rb
@@ -1,6 +1,6 @@
 class AddForkedFromIdToPatterns < ActiveRecord::Migration[5.2]
   def change
     add_column :patterns, :forked_from_id, :integer
-    add_reference :patterns, forked_from_id, :integer, index: true
+    add_reference :patterns, :forked_from_id, index: true
   end
 end
