55db9bde37ee43adc079534f6acd90d943ffefa7
curriecode <melaniecurrie01@gmail.com>
2019-11-24 22:28:49 -0800

    added some styling to the navbar

--- client/src/components/menu/menu.js
+++ client/src/components/menu/menu.js
@@ -1,30 +1,72 @@
+/* eslint-disable no-unused-expressions */
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav, Navbar, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 
 export default function Menu() {
+
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+
+    <Navbar expand="lg"
+      style={{
+        background: "radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)",
+        height: "150px"
+      }}>
+      <Navbar.Brand href="#home"
+        style={{
+          fontSize: "3.75rem",
+          fontWeight: "bolder"
+        }}>
+        Xstitch</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
+        <Nav className="mr-auto"
+          style={{
+            fontSize: "1.5em",
+            paddingTop: "25px"
+          }}>
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          <Nav.Link href="#link">Create</Nav.Link>
+          <NavDropdown title="Menu" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">My Patterns</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Favourites</NavDropdown.Item>
+            {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item> */}
+            {/* <NavDropdown.Divider /> */}
+            {/* <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
           </NavDropdown>
         </Nav>
-        <Form inline>
+        {/* <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-success">Search</Button>
-        </Form>
+        </Form> */}
+        <Button variant="outline-success"
+          style={{
+            color: "powderblue",
+            background: "#2884a7",
+            padding: "5px",
+            margin: "10px",
+            fontSize: "20px",
+            fontWeight: "bolder",
+            marginTop: "40px",
+            borderColor: "none"
+          }}>
+          Sign Up
+        </Button>
+        <Button variant="outline-success"
+          style={{
+            color: "powderblue",
+            background: "#2884a7",
+            padding: "5px",
+            margin: "10px",
+            fontSize: "20px",
+            fontWeight: "bolder",
+            marginTop: "40px",
+            borderColor: "none"
+          }}>
+          Login
+        </Button>
       </Navbar.Collapse>
-    </Navbar>
+    </Navbar >
   )
 }
