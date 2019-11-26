2d134e762c2212cef508864f0b96dd487fc27f52
curriecode <melaniecurrie01@gmail.com>
2019-11-26 12:33:43 -0800

    added: functionality to click away and hide the side menu

--- client/src/App.js
+++ client/src/App.js
@@ -7,49 +7,32 @@ import PatternList from './components/menu/patternList'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-// class App extends Component {
 export default function App() {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     message: 'Click the button to load data!',
-  //   }
-  // }
 
-  const [pattern, setPatternInfo] = useState("Welcome");
-
-  // const fetchData = () => {
-  //   axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-  //     .then((response) => {
-  //       // handle success
-  //       console.log(response.data) // The entire response from the Rails API
-
-  //       console.log(response.data.message) // Just the message
-  //       this.setState({
-  //         message: response.data.message
-  //       });
-  //     })
-  // };
-  // let render = () => {
+  let [pattern, setPatternInfo] = useState(false);
   let content;
-  if (pattern === "Welcome") {
+
+
+  //closes side menu when you click away
+  function clickOffMenu() {
+    if (pattern === true) {
+      setPatternInfo(false)
+    }
+  }
+
+  //opens side menu 
+  if (pattern === false) {
     content = <div></div>
   } else {
-    content = <PatternList />
+    content = <PatternList
+    />
   }
   return (
-    <div className="App">
+    <div className="App" onClick={clickOffMenu}>
       <Menu
         setPattern={setPatternInfo}
       />
       {content}
-      {/* <h1>{this.state.message}</h1>
-      <button onClick={this.fetchData} >
-        boop
-        </button> */}
     </div>
   );
-  // }
 }
-
-// export default App;
--- client/src/components/menu/menu.js
+++ client/src/components/menu/menu.js
@@ -31,7 +31,7 @@ export default function Menu(props) {
           <Nav.Link href="#link">Create</Nav.Link>
           <NavDropdown title="Menu" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1"
-              onClick={() => props.setPattern("MY_PATTERN")}
+              onClick={() => props.setPattern(true)}
               style={{
                 marginTop: "none",
                 fontSize: "20px",
@@ -52,15 +52,10 @@ export default function Menu(props) {
               {/* Need to ad functionality to open and close side menu */}
               Favourites
             </NavDropdown.Item>
-            {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item> */}
-            {/* <NavDropdown.Divider /> */}
-            {/* <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
+
           </NavDropdown>
         </Nav>
-        {/* <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form> */}
+
         <Button variant="outline-success"
 
           style={{
