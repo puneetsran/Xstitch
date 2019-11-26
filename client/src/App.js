796ba47cc528f6ada38972d93c5bcb3449294e9a
charcharmasonjar <charlotte.dibb@gmail.com>
2019-11-26 11:57:58 -0800

    Import edit component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 import './components/menu/menu'
 import Menu from './components/menu/menu';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Edit from "./components/edit/Edit"
 
-import Grid from "./components/Grid";
 
 class App extends Component {
   constructor(props) {
@@ -36,8 +36,8 @@ class App extends Component {
         <h1>{this.state.message}</h1>
         <button onClick={this.fetchData} >
           Fetch Data
-        </button>        
-        <Grid/>
+        </button>
+        <Edit />
       </div>
     );
   }
