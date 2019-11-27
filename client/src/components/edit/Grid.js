58c008d2e4072c84b932a48748a23ffca1871d93
charcharmasonjar <charlotte.dibb@gmail.com>
2019-11-26 18:40:18 -0800

    Remove unnecessary comment

--- client/src/components/edit/Grid.js
+++ client/src/components/edit/Grid.js
@@ -2,10 +2,6 @@ import React from "react";
 import Row from "./Row";
 import "./Grid.css";
 
-//if number of rows is increased, add row to the bottom
-//  do this by adding an array of transparent values the same length as the other arrays
-//if number of columns is increased, add a transparent value to the end of each array in the grid
-
 export default function Grid(props) {
 
   const rows = props.pattern.map((row, index) => {
