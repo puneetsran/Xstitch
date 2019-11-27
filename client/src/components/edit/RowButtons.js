a62b936922859594d1fb8af710666cb720f12f3c
charcharmasonjar <charlotte.dibb@gmail.com>
2019-11-26 18:39:19 -0800

    Create buttons to update number of rows

---
+++ client/src/components/edit/RowButtons.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button, Icon } from "semantic-ui-react";
+
+export default function RowButtons(props) {
+  return (
+    <Button.Group vertical>
+      <Button icon labelPosition="left" onClick={props.addRow}>
+        <Icon name="plus" />
+        Add Row
+      </Button>
+      <Button icon labelPosition="left">
+        Delete Row
+        <Icon name="minus" />
+      </Button>
+    </Button.Group>
+  );
+}
