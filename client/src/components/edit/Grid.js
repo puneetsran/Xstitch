8204540f68ee39ce0d911220b05cf85d7697bbc0
charcharmasonjar <charlotte.dibb@gmail.com>
2019-11-27 12:50:14 -0800

    Add basic styling to display grid component

--- client/src/components/edit/Grid.js
+++ client/src/components/edit/Grid.js
@@ -3,9 +3,19 @@ import Row from "./Row";
 import "./Grid.css";
 
 export default function Grid(props) {
-
   const rows = props.pattern.map((row, index) => {
-    return <Row key={index} row={index} values={row} updateColor={props.updateColor}/>;
+    return (
+      <Row
+        key={index}
+        row={index}
+        values={row}
+        updateColor={props.updateColor}
+      />
+    );
   });
-  return <section className="grid">{rows}</section>;
+  return (
+    <section className="grid">
+      <div id="capture">{rows}</div>
+    </section>
+  );
 }
