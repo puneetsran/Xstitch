9a56ab8fa991039a4732774bf7e413daa58c4dfb
Puneet Sran <puneet_sran@icloud.com>
2019-11-28 13:05:17 -0800

    add favourite and pattern controller in api folder, define create, show and desctroy functions for fav controller

--- app/controllers/api/patterns_controller.rb
+++ app/controllers/api/patterns_controller.rb
@@ -13,4 +13,20 @@ class Api::PatternsController < ApplicationController
     render json: { pattern: pattern }, status: 200 and return
   end
 
+  def create_pattern
+    @pattern = Pattern.new(pattern_params)
+    if @pattern.save
+      session[:pattern_id] = @pattern.id
+      redirect_to '/', notice: 'Pattern created successfully'
+    else
+      redirect_to '/home'
+    end
+  end
+
+  private
+
+  def pattern_params
+    params.require(:pattern).permit(:title, :description)
+  end
+
 end
--- client/src/components/patterns/patterns.js
+++ client/src/components/patterns/patterns.js
@@ -1,11 +1,14 @@
-import React, { useEffect, Component, useReducer, useState } from "react";
+// import React, { useEffect, Component, useReducer, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import styled from "styled-components";
+// import styled from "styled-components";
 import "./styles.css";
 // import Button from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { IoIosHeart, IoIosShareAlt } from "react-icons/io";
-import Pixel from "../edit/Pixel";
+// import Pixel from "../edit/Pixel";
 // import reducer from "reducers/application";
 
 // const PatternsContainer = styled.div``;
@@ -13,6 +16,12 @@ import Pixel from "../edit/Pixel";
 export default function Patterns(props) {
   // const [state, setState] = useState();
   // console.log("state here", state);
+
+  const currentUser = {
+    name: "John",
+    email: "John@email.com",
+    password: "password"
+  };
 
   function viewPattern(pattern) {
     // console.log("pattern here", pattern);
@@ -25,20 +34,49 @@ export default function Patterns(props) {
     };
   }
 
+  // does not work
   function addToFavourites(pattern) {
     return () => {
       return axios
         .get(`/api/patterns/${pattern.id}`)
         .then(({ data: { pattern } }) => {
           console.log("add this to favourite", pattern);
+          console.log("user", pattern.user_id);
         });
     };
   }
+  function sharePattern() {
+    const [show, setShow] = useState(false);
 
-  // function parseString(string) {
-  // }
+    const handleClose = () => setShow(false);
+    // const handleShow = () => setShow(true);
+
+    return (
+      <>
+        {/* <Button variant="primary" onClick={handleShow}>
+          Launch demo modal
+        </Button> */}
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
 
   // const pixelTest = "rgb(0, 0, 0)";
+
   const patterns = props.patterns.map(pattern => {
     return (
       <li className="card-deck" key={pattern.id}>
@@ -52,7 +90,10 @@ export default function Patterns(props) {
               {pattern.colours[0]}Pattern here
             </p>
             <div className="card-footer bg-transparent text-right">
-              <IoIosShareAlt className="share"></IoIosShareAlt>
+              <IoIosShareAlt
+                className="share"
+                onClick={sharePattern()}
+              ></IoIosShareAlt>
               <IoIosHeart
                 className="heart"
                 onClick={addToFavourites(pattern)}
