e03ec56d6e4adc75c3558fd3fae9da387fe6edcf
charcharmasonjar <charlotte.dibb@gmail.com>
2019-12-01 17:46:30 -0800

    Create View component

---
+++ client/src/components/view/View.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import ViewGrid from "./ViewGrid";
+import "./View.css";
+import { Button, Icon } from "semantic-ui-react";
+
+//default array for rendering grid
+const blankPattern = [];
+for (let i = 0; i < 20; i++) {
+  blankPattern.push([]);
+  for (let j = 0; j < 20; j++) {
+    blankPattern[i].push("#fff");
+  }
+}
+
+// needs to be set up with html2canvas to generate the preview
+function print() {
+  return;
+}
+
+export default function View(props) {
+  // will need to change this to load pattern passed in from props
+  const [pattern, updatePattern] = useState(blankPattern);
+
+  return (
+    <section className="view">
+      <ViewGrid pattern={pattern} />
+      <div className="view-buttons">
+        <Button.Group vertical>
+          <Button icon labelPosition="left" onClick={props.addColumn}>
+            <Icon name="heart" />
+            Favourite
+          </Button>
+          <Button icon labelPosition="left" onClick={props.addColumn}>
+            <Icon name="print" />
+            Print
+          </Button>
+          <Button icon labelPosition="left" onClick={props.addColumn}>
+            <Icon name="image" />
+            Save
+          </Button>
+          <Button icon color="green" labelPosition="left" onClick={print} floated="right">
+            Fork
+            <Icon name="fork" />
+          </Button>
+        </Button.Group>
+      </div>
+    </section>
+  );
+}
