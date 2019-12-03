ba476e9dac41861604c8a8c2bdfb771183bd6ba0
curriecode <melaniecurrie01@gmail.com>
2019-12-02 19:52:08 -0800

    removed conflicts from apicontroller

--- app/controllers/api/checkpoints_controller.rb
+++ app/controllers/api/checkpoints_controller.rb
@@ -1,46 +1,4 @@
 class Api::CheckpointsController < ApplicationController
-<<<<<<< HEAD
-  def index
-    render json: Checkpoint.all
-  end
-  
-  def show
-    @checkpoint = Checkpoint.find params[:id]
-    render json: {
-      checkpoint: @checkpoint
-    }
-  end
-
-  # def show
-  #   puts "INSIDE SHOW!!!!!"
-  #   p params
-  #   id = params[:id]
-  #   checkpoint = Checkpoint.find(id)
-  #   render json: { checkpoint: checkpoint}, status: 200 and return
-  # end
-
-  def create
-    puts "inside create checkpoint"
-    @checkpoint = Checkpoint.find_or_create_by(
-      user_id: params[:user_id],
-      pattern_id: params[:pattern_id],
-      colours: params[:colours],
-      image_url: params[:image_url]
-    )
-    @checkpoint.save
-
-    render json: {
-      id: @checkpoint.id
-    }
-  end
-
-  def destroy
-    @checkpoint = Checkpoint.find params[:id]
-    @checkpoint.destroy
-  end
-
-end
-=======
   # def index
  
 
@@ -93,4 +51,3 @@ end
     end
   end
 end
->>>>>>> merge
