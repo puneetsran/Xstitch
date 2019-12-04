671e6d8b72856ac46d30aeef80bc5bf56559a414
charcharmasonjar <charlotte.dibb@gmail.com>
2019-12-04 05:54:44 -0800

    Update pattern component to show pattern image and add view button

--- client/src/components/patterns/patterns.js
+++ client/src/components/patterns/patterns.js
@@ -1,19 +1,57 @@
-import React from "react";
-import "./styles.css";
-
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from "react";
+// import "./styles.css";
+import "./patterns.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, Icon, Image } from "semantic-ui-react";
 
 import Pattern from "./pattern";
+import beeImage from "./bee.jpg";
+
+function getPatternImages(patterns, checkpoints) {
+  return patterns.map(pattern => {
+    let matchingCheckpoints = checkpoints.filter(checkpoint => {
+      return checkpoint.pattern_id === pattern.id;
+    });
+    let mostRecentCheckpoint =
+      matchingCheckpoints[matchingCheckpoints.length - 1];
+    if (mostRecentCheckpoint) {
+      pattern.image_url = mostRecentCheckpoint.image_url;
+      return pattern;
+    } else {
+      pattern.image_url = null;
+      return pattern;
+    }
+  });
+}
 
 export default function Patterns(props) {
-  const patterns = props.patterns.map(pattern => {
-    return <Pattern key={pattern.id} pattern={pattern} />;
+  const [patternData, setPatternData] = useState([]);
+
+  useEffect(() => {
+    setPatternData(getPatternImages(props.patterns, props.checkpoints));
+  }, [props.patterns, props.checkpoints]);
+
+  // getPatternImages(props.patterns, props.checkpoints);
+
+  const patterns = patternData.map(pattern => {
+    return (
+      <Pattern
+        key={pattern.id}
+        pattern={pattern}
+        viewPage={props.setPage}
+        renderSavedPattern={props.renderSavedPattern}
+      />
+    );
   });
   return (
-    <div className="pattern-container">
-      <div className="patterns">
+    <div className="pattern-main-container">
+      <div className="hero-text">Welcome to Xstitch</div>
+      <img src={beeImage} className="bee-image" />
+      <div className="pattern-container">
+        {/* <div className="patterns"> */}
         {patterns}
         {/* <div className="pattern-row"></div> */}
+        {/* </div> */}
       </div>
     </div>
   );
